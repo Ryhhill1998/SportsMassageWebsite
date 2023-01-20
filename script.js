4b99223915c57bc0e7f1fbd9c3f84c15ccf98f08
ryhhill1998 <ryhhill1998@outlook.com>
2023-01-20 15:25:55 +0000

    Added javascript functionality to testimonials carousel

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const dropdown = document.getElementById("dropdown");
-const hamburger = document.getElementById("hamburger");
-const navbar = document.querySelector(".main-nav");
-
-const toggleDropdown = () => {
-    dropdown.classList.toggle("hidden");
-    toggleNavbarBorderBottomRadius();
-}
-
-const dropdownIsHidden = () => {
-    return dropdown.classList.contains("hidden");
-}
-
-const toggleNavbarBorderBottomRadius = () => {
-    if (dropdownIsHidden()) {
-        navbar.style.borderBottomRightRadius = "10px";
-        navbar.style.borderBottomLeftRadius = "10px";
-    } else {
-        navbar.style.borderBottomRightRadius = "0";
-        navbar.style.borderBottomLeftRadius = "0";
-    }
-}
-
-hamburger.addEventListener("click", toggleDropdown);
+// const dropdown = document.getElementById("dropdown");
+// const hamburger = document.getElementById("hamburger");
+// const navbar = document.querySelector(".main-nav");
+//
+// const toggleDropdown = () => {
+//     dropdown.classList.toggle("hidden");
+//     toggleNavbarBorderBottomRadius();
+// }
+//
+// const dropdownIsHidden = () => {
+//     return dropdown.classList.contains("hidden");
+// }
+//
+// const toggleNavbarBorderBottomRadius = () => {
+//     if (dropdownIsHidden()) {
+//         navbar.style.borderBottomRightRadius = "10px";
+//         navbar.style.borderBottomLeftRadius = "10px";
+//     } else {
+//         navbar.style.borderBottomRightRadius = "0";
+//         navbar.style.borderBottomLeftRadius = "0";
+//     }
+// }
+//
+// hamburger.addEventListener("click", toggleDropdown);
---
+++ testimonials.js
@@ -0,0 +1,45 @@
+const leftArrow = document.querySelector(".arrow--left");
+const rightArrow = document.querySelector(".arrow--right");
+const testimonials = document.querySelectorAll(".testimonial");
+let currentTestimonialIndex = 0;
+
+const changeSlide = (change) => {
+    hideTestimonial(currentTestimonialIndex);
+    change < 0 ? decrementSlideIndex() : incrementSlideIndex();
+    showTestimonial(currentTestimonialIndex);
+}
+
+const changeSlideLeft = () => {
+    changeSlide(-1);
+}
+
+const changeSlideRight = () => {
+    changeSlide(1);
+}
+
+const decrementSlideIndex = () => {
+    if (currentTestimonialIndex === 0) {
+        currentTestimonialIndex = 2;
+    } else {
+        currentTestimonialIndex--;
+    }
+}
+
+const incrementSlideIndex = () => {
+    if (currentTestimonialIndex === 2) {
+        currentTestimonialIndex = 0;
+    } else {
+        currentTestimonialIndex++;
+    }
+}
+
+const showTestimonial = (index) => {
+    testimonials[index].classList.remove("hidden");
+}
+
+const hideTestimonial = (index) => {
+    testimonials[index].classList.add("hidden");
+}
+
+leftArrow.addEventListener("click", changeSlideLeft);
+rightArrow.addEventListener("click", changeSlideRight);
