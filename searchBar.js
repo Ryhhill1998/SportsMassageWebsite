87e76e231d389617e6e1e0b1e7923584f985c35e
ryhhill1998 <ryhhill1998@outlook.com>
2023-01-24 09:24:41 +0000

    Added search bar functionality to website

--- searchBar.js
+++ searchBar.js
@@ -1,5 +1,17 @@
 const searchField = document.getElementById("search-field");
 const searchButton = document.getElementById("search-button");
+const searchResultsContainer = document.getElementById("search-results-container");
+
+const possibleSearches = [
+    "home",
+    "sports massage",
+    "reflexology",
+    "swedish massage",
+    "indian head massage",
+    "la stone therapy",
+    "corporate pamper days",
+    "peer-to-peer massages",
+];
 
 const searchResultLinks = {
     "home": "index.html",
@@ -9,16 +21,44 @@ const searchResultLinks = {
     "indian head massage": "indianHeadMassage.html",
     "la stone therapy": "stoneTherapy.html",
     "corporate pamper days": "pamperDays.html",
-    "peer to peer massages": "peerToPeer.html",
+    "peer-to-peer massages": "peerToPeer.html",
+}
+
+const createSearchResult = (result) => {
+    const html = `
+        <li>
+            <a class="search-result" href=${searchResultLinks[result]}>
+                ${result}
+            </a>
+        </li>
+    `;
+
+    searchResultsContainer.insertAdjacentHTML("beforeend", html);
+}
+
+const displayResults = (results) => {
+    searchResultsContainer.innerHTML = "";
+    results.forEach(result => createSearchResult(result));
+}
+
+const findSearchResults = (query) => {
+    return possibleSearches.filter((search) => search.includes(query));
 }
 
 const processSearch = () => {
-    const searchQuery = searchField.value;
-    const resultLink = searchResultLinks[searchQuery];
+    const searchQuery = searchField.value.toLowerCase();
+    console.log(searchQuery);
 
-    if (!resultLink) return;
+    if (searchQuery.trim() === "") return;
+    console.log(searchQuery)
 
-    location.href = searchResultLinks[searchQuery];
+    const results = findSearchResults(searchQuery);
+
+    console.log(results);
+
+    if (!results.length) return;
+
+    displayResults(results);
 }
 
 searchButton.addEventListener("click", processSearch);
