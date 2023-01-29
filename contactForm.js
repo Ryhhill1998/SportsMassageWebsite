720f6222f5ce38d71524d5466bdb5c7f1cbe1685
ryhhill1998 <ryhhill1998@outlook.com>
2023-01-29 10:23:31 +0000

    Added functionality to stop scrolling when modal is open

--- contactForm.js
+++ contactForm.js
@@ -2,6 +2,7 @@ const overlay = document.querySelector(".overlay");
 const contactButtons = document.querySelectorAll(".contact-button");
 const contactForm = document.querySelector(".contact-form");
 const closeFormButton = document.getElementById("close-form-button");
+const body = document.querySelector("body");
 
 const showContactForm = () => {
     if (!dropdownMenu.classList.contains("hidden")) {
@@ -9,11 +10,13 @@ const showContactForm = () => {
     }
     contactForm.style.display = "block";
     overlay.style.display = "block";
+    body.style.overflow = "hidden";
 }
 
 const hideContactForm = ({target}) => {
     contactForm.style.display = "none";
     overlay.style.display = "none";
+    body.style.overflow = "auto";
 }
 
 contactButtons.forEach(button => button.addEventListener("click", showContactForm));
